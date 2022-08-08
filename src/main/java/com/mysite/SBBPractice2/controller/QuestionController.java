7949d68e9a9848acc665c69a2b9ac8eac34fb7ae
ONEASH <oneash0824@naver.com>
2022-08-08 22:37:29 +0900

    Question Detail page

--- src/main/java/com/mysite/SBBPractice2/controller/QuestionController.java
+++ src/main/java/com/mysite/SBBPractice2/controller/QuestionController.java
@@ -2,24 +2,34 @@ package com.mysite.SBBPractice2.controller;
 
 import com.mysite.SBBPractice2.domain.Question;
 import com.mysite.SBBPractice2.repository.QuestionRepository;
+import com.mysite.SBBPractice2.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
---
+++ src/main/java/com/mysite/SBBPractice2/service/QuestionService.java
@@ -0,0 +1,32 @@
+package com.mysite.SBBPractice2.service;
+
+
+import com.mysite.SBBPractice2.DataNotFoundException;
+import com.mysite.SBBPractice2.domain.Question;
+import com.mysite.SBBPractice2.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
