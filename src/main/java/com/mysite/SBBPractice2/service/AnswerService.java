f629c58c8ac6c282e79663a078193c6b418b3ab6
ONEASH <oneash0824@naver.com>
2022-08-18 22:08:44 +0900

    Answer Modify & Delete

--- src/main/java/com/mysite/SBBPractice2/service/AnswerService.java
+++ src/main/java/com/mysite/SBBPractice2/service/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mysite.SBBPractice2.service;
 
+import com.mysite.SBBPractice2.DataNotFoundException;
 import com.mysite.SBBPractice2.domain.Answer;
 import com.mysite.SBBPractice2.domain.Question;
 import com.mysite.SBBPractice2.domain.SiteUser;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.nio.Buffer;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +40,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
